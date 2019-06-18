30c247d7186f758346322cbd4dadf152c022b5d2
programer10 <programer10@ucione.local>
2019-06-18 21:30:12 +0200

    Loader

--- src/js/base/login.js
+++ src/js/base/login.js
@@ -4,8 +4,48 @@ let handleLogin = function(options){
             $('#loginForm').on('submit', function(e){
                 e.preventDefault();
 
+                let _thisloginForm = $(this);                
                 let data = $(this).getFormData();
-                console.log(data);
+                
+                $.ajax({
+                    crossDomain: true,
+                    url: 'data/login.json',
+                    method: 'POST',
+                    contentType: 'application/json',
+                    headers: {
+                        'Access-Controll-Allow-Origin': '*'
+                    },
+                    data: JSON.stringify(data),
+                    beforeSend: function(){
+                        $('#loginModal .modal-content').prepend(getLoader());
+                        $('button', _thisloginForm).prop('disabled', true);
+                    },
+                    success: function(data){
+                        if(data.status == 0){
+                            $('#loginModal .loader').append(setAjaxMessage(data.message));
+                        } else if(data.status == 1){
+                            $('#loginModal .loader').append(setAjaxMessage(data.message));
+                            setTimeout(function(){
+                                window.location('/admin');
+                            }, 2000);
+                        }
+                    },
+                    error: function(xhr){
+                        $('#loginModal .loader').append(setAjaxMessage('Došlo je do pogreške! Pokušajte ponovo ili kontaktirajte administratora!'));
+                    },
+                    complete: function(){
+                        setTimeout(function(){
+                            $('.loader').slideUp(700);
+                            $('button', _thisloginForm).prop('disabled', true);
+                        }, 2500);
+                    }
+                });
+            });
+
+            $('#loginModal').on('hidden.bs.modal', function(e){
+                e.preventDefault();
+
+                $('#loginForm').trigger('reset').parsley().reset();
             });
         }
 
